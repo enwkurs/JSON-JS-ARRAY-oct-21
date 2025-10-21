78de328f09e980c65de5dd90dab937f497e073e4
enwkurs <erinwajca@gmail.com>
2025-10-21 13:18:10 +0200

    fetch api

--- script.js
+++ script.js
@@ -23,29 +23,54 @@
 
 
 // Async functions
-async function gotHouses() {
-    // Refer to the json file
-    const data = await fetch("houses.json");
+// async function gotHouses() {
+//     // Refer to the json file
+//     const data = await fetch("houses.json");
+//     console.log(data);
+
+//     // Get the data from the json file
+//     const response = await data.json();
+//     console.log(response);
+//     console.log(response["House Stark"]["Jon Snow"].title);
+//     console.log(response["House Targaryen"]["Daenerys Targaryen"].title);
+//     // Follow the structure of the data, naming each step
+//     // Like navigating folders
+//     // Use ".word" to refer to specific keys
+
+//     const gotCharacters = document.querySelector("#gotCharacters");
+//     gotCharacters.innerHTML = `
+//     <h1>${response["House Stark"]["Jon Snow"].nickname}</h1>
+//     <h3>${response["House Stark"]["Jon Snow"].alias}</h3>
+//     <p>${response["House Stark"]["Jon Snow"].title}</p>
+//     `;
+
+// };
+
+// gotHouses();
+
+// SEE UPDATED CODE IN LESSON UPLOAD
+
+
+
+//
+//
+//
+
+async function catFacts() {
+    const data = await fetch("https://catfact.ninja/facts");
     console.log(data);
 
-    // Get the data from the json file
     const response = await data.json();
     console.log(response);
-    console.log(response["House Stark"]["Jon Snow"].title);
-    console.log(response["House Targaryen"]["Daenerys Targaryen"].title);
-    // Follow the structure of the data, naming each step
-    // Like navigating folders
-    // Use ".word" to refer to specific keys
+    console.log(response.data);
+    console.log(response.data[0]);
+    console.log(response.data[0].fact);
 
-    const gotCharacters = document.querySelector("#gotCharacters");
-    gotCharacters.innerHTML = `
-    <h1>${response["House Stark"]["Jon Snow"].nickname}</h1>
-    <h3>${response["House Stark"]["Jon Snow"].alias}</h3>
-    <p>${response["House Stark"]["Jon Snow"].title}</p>
-    `;
+    console.log("---------------For Loop Setup---------------");
+    for( let index = 0; index < response.data.length; index++ ){
+        console.log(response.data[index]);
+    };
 
 };
 
-gotHouses();
-
-// SEE UPDATED CODE IN LESSON UPLOAD
+catFacts();
